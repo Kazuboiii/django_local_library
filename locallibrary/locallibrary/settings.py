ff3fecba0393b5f7273b33861969d8bea5a33bf0
Kazuboiiiiiiiii <bhattaraisudin211@gmail.com>
2025-07-13 23:07:35 +0545

    Templating

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!e*xfjg$r^-5x$1a236a=a(iko#_2sak2^c8&18j3awj4t!t1!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
